29a498b46e6b01077ea8267fee9d8b0bf8bfd6ad
pgambino-espear <pgambino@espear.com>
2020-05-13 17:08:36 -0400

    Finished implementing DLLs.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -62,7 +62,7 @@ class DoublyLinkedList:
         return self.head
 
 
-    """Removes the List's current head node, making the
+    """Removes the List's current head node, makinbgg the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
@@ -126,18 +126,46 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return None
+        value = node.value
+        if node is self.tail:
+            self.remove_from_tail()
+        else:
+            node.delete()
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return None
+        value = node.value
+        if node is self.head:
+            self.remove_from_tail()
+        else:
+            node.delete()
+        self.add_to_tail(value)
+
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
-        
+        if not self.head and not self.tail:
+            return None
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+
     """Returns the highest value currently in the list"""
     def get_max(self):
         pass
