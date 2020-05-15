3c6045770d5b17ce6bfcbc4d8b68efaa7251ecad
pgambino-espear <pgambino@espear.com>
2020-05-14 23:12:44 -0400

    Finished binary search tree problems.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,11 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+from collections import deque
+
+from stack import Stack
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,37 +22,102 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:    
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+        
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # if not node.left:
+        #     print(node.value)
+        # else:
+        #     return node.left.in_order_print 
+        if node is None:
+            return node
+        self.in_order_print(node.left)
+        print(node.value)
+        self.in_order_print(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = deque()
+        queue.append(self)
+        while len(queue) > 0:
+            currentNode = queue.popleft()
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+            print(currentNode)
+        #create queue
+        #add root to queue
+        #While queue is not empty:
+        # node = head of queue
+        #Do the thing (print in this case)
+        #Add children of node to queue
+        #pop node off the queu
+        # Print the value of every node, starting with the given node,
+        # in an iterative depth first traversal
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        arr = []
+        arr.append(node)
+        while len(arr) > 0:
+            currentNode = arr.pop()
+            print(currentNode.value)
+            if currentNode.left:
+                arr.append(currentNode.left)
+            if currentNode.right:
+                arr.append(currentNode.right)
+
+        
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
@@ -59,3 +129,7 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+firstNode = BSTNode(1)
+secondNode = BSTNode(2)
+firstNode.insert(0)
